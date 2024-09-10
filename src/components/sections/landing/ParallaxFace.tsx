7e23728e62bd6219b49b1f14a6fd87e7e043e5bb
Nico Wätzig <42875398+Nico2d@users.noreply.github.com>
2024-09-10 13:43:24 +0200

    fix: add focusArea

--- src/components/sections/landing/ParallaxFace.tsx
+++ src/components/sections/landing/ParallaxFace.tsx
@@ -4,28 +4,84 @@ import face2 from '@images/face/LandingFace-part2.png'
 import face3 from '@images/face/LandingFace-part3.png'
 import { ParallaxLayer } from './ParallaxLayer'
 import { useWindowSize } from '@/hooks/useWindowSize'
+import { distanceFromFocusArea } from '@/utils/countDistance'
+import { DistanceType } from '@/types/types'
 
 export const ParallaxFace = ({ isLocked = false }) => {
-	const FOCUS_POINT_OFFSET = 140
 	const BREAK_POINT = 1024
+	const FOCUS_POINT_OFFSET = 100
+	const FOCUS_AREA = 80
 
-	const [focusPoint, setFocusPoint] = useState([0, 0])
+	const [distance, setDistance] = useState<DistanceType>({
+		distance: 0,
+		distanceX: 0,
+		distanceY: 0,
+	})
 	const resolution = useWindowSize()
 
-	// TODO: add focusArea instead of focusPoint
-	// const [focusArea, setFocusArea] = useState([
-	// 	[0, 0],
-	// 	[0, 10],
-	// 	[10, 0],
-	// 	[10, 10],
-	// ])
+	const [focusArea, setFocusArea] = useState([
+		[0, 0],
+		[0, 0],
+		[0, 0],
+		[0, 0],
+	])
 
 	useEffect(() => {
-		setFocusPoint([
-			FOCUS_POINT_OFFSET,
-			window.innerHeight - FOCUS_POINT_OFFSET,
+		setFocusArea([
+			[FOCUS_POINT_OFFSET, window.innerHeight - FOCUS_POINT_OFFSET],
+			[
+				FOCUS_POINT_OFFSET + FOCUS_AREA,
+				window.innerHeight - FOCUS_POINT_OFFSET,
+			],
+			[
+				FOCUS_POINT_OFFSET + FOCUS_AREA,
+				window.innerHeight - (FOCUS_POINT_OFFSET + FOCUS_AREA),
+			],
+			[
+				FOCUS_POINT_OFFSET,
+				window.innerHeight - (FOCUS_POINT_OFFSET + FOCUS_AREA),
+			],
 		])
 	}, [])
+
+	useEffect(() => {
+		const handleMouseMove = (e) => {
+			const { pageX, pageY } = e
+
+			const distance = distanceFromFocusArea(focusArea, [pageX, pageY])
+			setDistance(distance)
+		}
+
+		const subscribeMovement = () => {
+			window.addEventListener('mousemove', handleMouseMove)
+		}
+
+		const unsubscribeMovement = () => {
+			window.removeEventListener('mousemove', handleMouseMove)
+		}
+
+		if (isLocked) {
+			setDistance({
+				distance: 0,
+				distanceX: 0,
+				distanceY: 0,
+			})
+		} else {
+			subscribeMovement()
+		}
+
+		return () => {
+			unsubscribeMovement()
+		}
+	}, [focusArea, isLocked])
+
+	const getTransform = (speed: number) => {
+		const x = (distance.distanceX * speed) / 100
+		const y = (distance.distanceY * speed) / 100
+		const scale = 1 + Math.abs((distance.distance * speed) / 10000)
+
+		return `translateX(${x}px) translateY(${y}px) scale(${scale})`
+	}
 
 	return (
 		<div
@@ -33,37 +89,25 @@ export const ParallaxFace = ({ isLocked = false }) => {
 				isLocked ? 'z-30' : 'right-section z-0'
 			} relative lg:w-3/4 h-3/4 lg:h-full`}
 		>
-			<ParallaxLayer
-				speed={5}
-				focusPoint={focusPoint}
-				isLock={isLocked || resolution.width < BREAK_POINT}
-			>
+			<ParallaxLayer transform={getTransform(5)}>
 				<img
+					className={`aspect-square`}
 					src={face1.src}
 					alt="Layer 1"
-					className={`aspect-square`}
 				/>
 			</ParallaxLayer>
-			<ParallaxLayer
-				speed={-60}
-				focusPoint={focusPoint}
-				isLock={isLocked || resolution.width < BREAK_POINT}
-			>
+			<ParallaxLayer transform={getTransform(-60)}>
 				<img
+					className={`aspect-square`}
 					src={face2.src}
 					alt="Layer 2"
-					className={`aspect-square`}
 				/>
 			</ParallaxLayer>
-			<ParallaxLayer
-				speed={40}
-				focusPoint={focusPoint}
-				isLock={isLocked || resolution.width < BREAK_POINT}
-			>
+			<ParallaxLayer transform={getTransform(40)}>
 				<img
+					className={`aspect-square`}
 					src={face3.src}
 					alt="Layer 3"
-					className={`aspect-square`}
 				/>
 			</ParallaxLayer>
 		</div>
